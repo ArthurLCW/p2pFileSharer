111e0094af0059c0d018f4cab9f4346086a135b4
changwenl <changwenl@student.unimelb.edu.au>
2022-09-18 23:18:42 +1000

    Project1Submission

--- src/main/java/comp90015/idxsrv/Filesharer.java
+++ src/main/java/comp90015/idxsrv/Filesharer.java
@@ -64,7 +64,6 @@ public class Filesharer {
     	 */
     	InetAddress address = InetAddress.getByName("localhost"); // default hostname of this peer
     	InetAddress idxSrvAddress = InetAddress.getByName("localhost"); // default hostname of the index server
-		// InetAddress idxSrvAddress = InetAddress.getByName("172.26.130.185"); // hostname of the cloud index server
 		int idxSrvPort = 3200; // the port of the index server
 		int port = 3201; // the port this peer uses for other peers to connect to
 		int timeout = 1000; // the default socket timeout in milliseconds for idle sockets
